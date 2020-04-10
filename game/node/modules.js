6bc3818ffc7006d3a3a3dd54753edde8aaa6ef1a
gavinhsmith <gavindagrate101@gmail.com>
2020-04-09 21:41:43 -0500

    UD

--- game/node/modules.js
+++ game/node/modules.js
@@ -2,7 +2,7 @@ const {ipcRenderer,shell} = require('electron');
 const fs = require('fs');
 const imgconvert = require('image-data-uri');
 
-function screenshot(filename) {
+function screenshot(filepath) {
     var td = imgconvert.decode(c.toDataURL());
-    fs.writeFileSync(`C:\\Users\\gavin\\Repositories\\Un-Named-Platformer\\dist\\img\\${filename}.png`,td.dataBuffer);
+    fs.writeFileSync(filepath,td.dataBuffer);
 };
